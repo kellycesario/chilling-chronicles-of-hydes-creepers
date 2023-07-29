27ba5e4bf423f961eaaf98b379a8462f3050ced2
kellycesario <kellycesario@gmail.com>
2023-07-29 16:50:21 -0300

    chore: add new preview properties (backgrounds and its values)

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -1,15 +1,37 @@
-import type { Preview } from "@storybook/react";
+import '@/styles/main.scss'
+import type { Preview } from '@storybook/react'
 
 const preview: Preview = {
   parameters: {
-    actions: { argTypesRegex: "^on[A-Z].*" },
+    actions: { argTypesRegex: '^on[A-Z].*' },
     controls: {
       matchers: {
         color: /(background|color)$/i,
         date: /Date$/,
       },
     },
+    backgrounds: {
+      default: '',
+      values: [
+        {
+          name: 'dark-mode',
+          value: '#000706',
+        },
+        {
+          name: 'light-gray',
+          value: '#f5f7f',
+        },
+        {
+          name: 'purple-mode',
+          value: '#765ba8',
+        },
+        {
+          name: 'linear-purple-mode',
+          value: 'linear-gradient(180deg, #c4c2ff 0%, #e5e5e5 100%)',
+        },
+      ],
+    },
   },
-};
+}
 
-export default preview;
+export default preview
