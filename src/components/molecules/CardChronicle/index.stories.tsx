c1ef449fe2d437ad4078fb294d9f48442d33e91c
kellycesario <kellycesario@gmail.com>
2023-07-30 19:32:31 -0300

    add: create card chronicle molecule

---
+++ src/components/molecules/CardChronicle/index.stories.tsx
@@ -0,0 +1,56 @@
+import { Sprites } from '@/components/ions/Sprites'
+import { CardChronicle } from './index'
+
+export default {
+  component: CardChronicle,
+  title: 'Molecules/CardChronicle',
+  tags: ['autodocs'],
+  parameters: {
+    layout: 'centered',
+  },
+  args: {
+    ariaLabel: 'Card',
+    author: 'Daisy Jones',
+    href: 'www.google.com.br',
+    image:
+      'https://media-cldnry.s-nbcnews.com/image/upload/newscms/2021_42/1792285/pumpkin-shortage-te-main-211022.jpg',
+    size: '',
+    subtitle: 'This is a very nice subtitle for this article in question',
+    title: 'Article title',
+  },
+  decorators: [
+    (Story) => {
+      return (
+        <div>
+          <Sprites />
+          <Story />
+        </div>
+      )
+    },
+  ],
+  argTypes: {
+    ariaLabel: {
+      table: {
+        disable: true,
+      },
+    },
+    href: {
+      table: {
+        disable: true,
+      },
+    },
+    image: {
+      table: {
+        disable: true,
+      },
+    },
+  },
+}
+
+export const Default = {}
+
+export const Large = {
+  args: {
+    size: 'large',
+  },
+}
---
+++ src/components/molecules/CardChronicle/index.tsx
@@ -0,0 +1,49 @@
+import { Headings } from '@/components/atoms/Headings'
+import { Text } from '@/components/atoms/Text'
+import { limitCharacters } from '@/utils/limitCharacters.js'
+import Link from 'next/link'
+import styles from './styles.module.scss'
+
+type CardChronicleProps = {
+  ariaLabel: string
+  author: string
+  href?: string
+  image: string
+  size: string
+  subtitle: string
+  title: string
+}
+
+export const CardChronicle = ({
+  ariaLabel,
+  author,
+  href,
+  image,
+  size,
+  subtitle,
+  title,
+}: CardChronicleProps) => {
+  const classList = [styles[`card`], styles[`card--${size}`]]
+  const background = {
+    backgroundImage: `url(${image})`,
+  }
+  const characterLimit = size === 'large' ? 60 : size === '' ? 30 : 33
+  const sub = limitCharacters(subtitle, characterLimit)
+
+  return (
+    <Link href={href} aria-label={ariaLabel}>
+      <article className={classList.join(' ')} style={background}>
+        <div className={styles.card__container}>
+          <Headings align='left' children={title} color='white' level='3' />
+          <Text align='left' children={sub} color='white' />
+          <div className={styles.card__author}>
+            <svg width='32' height='32' viewBox='0 0 32 32' fill='white'>
+              <use xlinkHref='#ghost' href='#ghost' />
+            </svg>
+            <Text align='left' children={author} color='white' />
+          </div>
+        </div>
+      </article>
+    </Link>
+  )
+}
