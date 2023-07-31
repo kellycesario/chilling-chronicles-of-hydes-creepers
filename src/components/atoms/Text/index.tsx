e30c8483fd5dc418b456ca3f965d74ce014e7d1c
kellycesario <kellycesario@gmail.com>
2023-07-31 09:44:31 -0300

    perf: create a new prop (weight)

--- src/components/atoms/Text/index.tsx
+++ src/components/atoms/Text/index.tsx
@@ -3,10 +3,16 @@ import styles from './styles.module.scss'
 type TextProps = {
   align: string
   children: string
-  color: string
+  color?: string
+  weight?: string
 }
-export const Text = ({ align, children, color }: TextProps) => {
-  const classList = [styles[`text`], styles[`text--${align}`], styles[`text--${color}`]]
+export const Text = ({ align, children, color, weight }: TextProps) => {
+  const classList = [
+    styles[`text`],
+    styles[`text--${align}`],
+    styles[`text--${color}`],
+    styles[`text--${weight}`],
+  ]
 
   return <p className={classList.join(' ')}>{children}</p>
 }
