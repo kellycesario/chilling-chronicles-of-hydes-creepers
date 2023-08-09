cf865fa250e7c3cb1c4a0bd3b66ff1f51eaf692c
kellycesario <kellycesario@gmail.com>
2023-08-09 09:55:12 -0300

    feat: create contact organism

---
+++ src/components/organisms/Contact/index.tsx
@@ -0,0 +1,28 @@
+import { Input } from '@/components/molecules/Input'
+import styles from './styles.module.scss'
+
+import { Button } from '@/components/atoms/Button'
+import { Headings } from '@/components/atoms/Headings'
+import contact from '@/data/contact.json'
+
+export const Contact = () => {
+  return (
+    <section className={styles.contact}>
+      <Headings align='center' children='Summon us' color='white' level='2' />
+      <form className={styles.contact__form}>
+        {contact.map((item, index) => (
+          <Input
+            key={index}
+            icon={item.icon}
+            id={item.id}
+            isTextarea={item.isTextarea}
+            label={item.label}
+            placeholder={item.placeholder}
+            type={item.type}
+          />
+        ))}
+        <Button label='Submit' level='tertiary' />
+      </form>
+    </section>
+  )
+}
