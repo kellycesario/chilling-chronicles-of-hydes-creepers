86ce6e63e41c3f269f3d02ff12ea0eadc3ad80f1
kellycesario <kellycesario@gmail.com>
2023-08-09 19:57:55 -0300

    fix: use local data to map in select molecule

--- src/components/molecules/Select/index.tsx
+++ src/components/molecules/Select/index.tsx
@@ -1,11 +1,18 @@
 import styles from './styles.module.scss'
 
+import topics from '@/data/topics.json'
+
 type SelectProps = {
   label: string
+  image: string
   options?: { value: string; label: string }[]
 }
 
-export const Select = ({ label, options }: SelectProps) => {
+export const Select = ({ label, image, options }: SelectProps) => {
+  const background = {
+    backgroundImage: `url(${image})`,
+  }
+
   return (
     <div className={styles.select}>
       <label htmlFor='topics' className={styles.select__label}>
@@ -15,11 +22,12 @@ export const Select = ({ label, options }: SelectProps) => {
         id='topics'
         aria-describedby='select a topic to filter the search'
         className={styles.select__dropdown}
+        style={background}
       >
-        {options &&
-          options.map((option) => (
-            <option key={option.value} value={option.value}>
-              {option.label}
+        {topics &&
+          topics.map((item, index) => (
+            <option key={index} value={item.value}>
+              {item.label}
             </option>
           ))}
       </select>
