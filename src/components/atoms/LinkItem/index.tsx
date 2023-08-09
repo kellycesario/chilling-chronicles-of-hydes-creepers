8e75407f10b335f21ae2604dc63825657eabb3a9
kellycesario <kellycesario@gmail.com>
2023-08-09 09:59:40 -0300

    refactor: add icon verification in link item atom

--- src/components/atoms/LinkItem/index.tsx
+++ src/components/atoms/LinkItem/index.tsx
@@ -2,14 +2,22 @@ import Link from 'next/link'
 import styles from './styles.module.scss'
 
 type LinkItemProps = {
-  children: string
-  href: string
+  children?: React.ReactNode
+  href?: string
+  hasIcon?: boolean
+  icon?: string
 }
 
-export const LinkItem = ({ children, href }: LinkItemProps) => {
-  return (
-    <li className={styles.link}>
+export const LinkItem = ({ children, href, hasIcon, icon }: LinkItemProps) => (
+  <li className={styles.link}>
+    {hasIcon ? (
+      <a href={href} aria-label={icon ? `Share this on ${icon}` : undefined}>
+        <svg width='32' height='32' viewBox='0 0 32 32' fill='#000706'>
+          <use xlinkHref={`#${icon}`} href={`#${icon}`} />
+        </svg>
+      </a>
+    ) : (
       <Link href={href}>{children}</Link>
-    </li>
-  )
-}
+    )}
+  </li>
+)
