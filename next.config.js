eb0503043934a9d2f3c1a48e6282111993b6fa6b
kellycesario <kellycesario@gmail.com>
2023-09-06 13:33:04 -0300

    fix: import index.scss globally

--- next.config.js
+++ next.config.js
@@ -1,4 +1,11 @@
+const path = require('path')
+
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  sassOptions: {
+    includePaths: [path.join(__dirname, '.src/styles')],
+    prependData: `@import "./src/styles/index.scss";`,
+  },
+}
 
 module.exports = nextConfig
