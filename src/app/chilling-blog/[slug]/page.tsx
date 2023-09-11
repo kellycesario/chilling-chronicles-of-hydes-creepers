20da116be4ca76f3f5caa2cb23a3029709e7fd78
kellycesario <kellycesario@gmail.com>
2023-09-11 16:31:36 -0300

    feat: prepare the page to receive contentful data

--- src/app/chilling-blog/[slug]/page.tsx
+++ src/app/chilling-blog/[slug]/page.tsx
@@ -1,55 +1,100 @@
+import { Metadata, ResolvingMetadata } from 'next'
+import { draftMode } from 'next/headers'
+import { notFound } from 'next/navigation'
+
 import { Accordion } from '@/components/organisms/Accordion'
 import { ArticleContent } from '@/components/organisms/ArticleContent'
 
-type ArticleContentProps = {
-  reviewer: string
-  date: string
-  alt: string
-  lead: string
-  picture: string
-  subtitle: string
-  description: string
-  emphasis: string
-  quote: string
-  firstParagraph: string
-  secondParagraph: string
-  thirdParagraph: string
-  fourthParagraph: string
+import { fetchChronicle, fetchChronicles } from '@/contentful/chroniclePosts'
+
+import { documentToPlainTextString } from '@contentful/rich-text-plain-text-renderer'
+
+interface ChronicleParams {
+  slug: string
 }
 
-export default function Article({
-  reviewer,
-  date,
-  alt,
-  lead,
-  picture,
-  subtitle,
-  description,
-  emphasis,
-  quote,
-  firstParagraph,
-  secondParagraph,
-  thirdParagraph,
-  fourthParagraph,
-}: ArticleContentProps) {
+interface ChronicleProps {
+  params: ChronicleParams
+}
+
+export async function generateStaticParams(): Promise<ChronicleParams[]> {
+  const chronicles = await fetchChronicles({ preview: false })
+
+  return chronicles.map((post) => ({ slug: post.slug }))
+}
+
+export async function generateMetadata(
+  { params }: ChronicleProps,
+  parent: ResolvingMetadata
+): Promise<Metadata> {
+  const chronicle = await fetchChronicle({
+    slug: params.slug,
+    preview: draftMode().isEnabled,
+  })
+
+  if (!chronicle) {
+    return notFound()
+  }
+
+  return {
+    title: chronicle.lead,
+  }
+}
+
+async function Article({ params }: ChronicleProps) {
+  const chronicle = await fetchChronicle({
+    slug: params.slug,
+    preview: draftMode().isEnabled,
+  })
+
+  if (!chronicle) {
+    return notFound()
+  }
+
+  let imageUrl: string | undefined = ''
+
+  if (chronicle.picture && 'fields' in chronicle.picture) {
+    imageUrl = `https:${chronicle.picture.fields.file.url}`
+  } else if (typeof chronicle.picture === 'string') {
+    imageUrl = chronicle.picture
+  }
+
   return (
     <section>
       <ArticleContent
-        reviewer={reviewer}
-        date={date}
-        alt={alt}
-        lead={lead}
-        picture={picture}
-        subtitle={subtitle}
-        description={description}
-        emphasis={emphasis}
-        quote={quote}
-        firstParagraph={firstParagraph}
-        secondParagraph={secondParagraph}
-        thirdParagraph={thirdParagraph}
-        fourthParagraph={fourthParagraph}
+        reviewer={chronicle.reviewer}
+        date={chronicle.date}
+        alt={chronicle.alt}
+        lead={chronicle.lead}
+        picture={imageUrl}
+        subtitle={chronicle.subtitle}
+        description={chronicle.description}
+        emphasis={chronicle.emphasis}
+        quote={chronicle.quote}
+        firstParagraph={
+          typeof chronicle.firstParagraph === 'string'
+            ? chronicle.firstParagraph
+            : documentToPlainTextString(chronicle.firstParagraph)
+        }
+        secondParagraph={
+          typeof chronicle.secondParagraph === 'string'
+            ? chronicle.secondParagraph
+            : documentToPlainTextString(chronicle.secondParagraph)
+        }
+        thirdParagraph={
+          typeof chronicle.thirdParagraph === 'string'
+            ? chronicle.thirdParagraph
+            : documentToPlainTextString(chronicle.thirdParagraph)
+        }
+        fourthParagraph={
+          typeof chronicle.fourthParagraph === 'string'
+            ? chronicle.fourthParagraph
+            : documentToPlainTextString(chronicle.fourthParagraph)
+        }
       />
       <Accordion />
     </section>
   )
 }
+
+export default Article
