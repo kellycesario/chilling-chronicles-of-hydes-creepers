5083b9b92f3526ddfcf21f48a430883d54127819
kellycesario <kellycesario@gmail.com>
2023-09-23 23:32:28 -0300

    fix: update filter logic

--- src/components/organisms/Filter/index.tsx
+++ src/components/organisms/Filter/index.tsx
@@ -3,6 +3,9 @@
 import { FilterItem } from '@/components/molecules/FilterItem'
 import { Select } from '@/components/molecules/Select'
 import { useEffect, useState } from 'react'
+import styles from './styles.module.scss'
+
+import topics from '@/data/topics.json'
 
 export const Filter = () => {
   const [isMobile, setIsMobile] = useState(window.innerWidth < 1200)
@@ -22,8 +25,15 @@ export const Filter = () => {
   return isMobile ? (
     <Select image='/arrow.svg' label='Search by topic' />
   ) : (
-    <ul>
-      <FilterItem icon='books' label='This is a topic' />
+    <ul className={styles.filter}>
+      {topics.map((item, index) => (
+        <FilterItem
+          key={index}
+          icon={item.icon}
+          label={item.label}
+          value={item.value}
+        />
+      ))}
     </ul>
   )
 }
