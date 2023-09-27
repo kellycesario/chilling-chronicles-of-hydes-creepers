ae41547abf081d7db3d1d77c00c5ef52e303a9d4
kellycesario <kellycesario@gmail.com>
2023-09-26 21:49:01 -0300

    feat: create search logic

--- src/components/molecules/SearchInput/index.tsx
+++ src/components/molecules/SearchInput/index.tsx
@@ -1,17 +1,42 @@
+'use client'
+
+import { useEffect, useState } from 'react'
 import { Input } from '../Input'
 import styles from './styles.module.scss'
 
-type SearchProps = {}
+type SearchProps = {
+  onSearch: (query: string) => void
+}
 
-export const SearchInput = ({}: SearchProps) => {
+export const SearchInput = ({ onSearch }: SearchProps) => {
+  const [searchQuery, setSearchQuery] = useState('')
+
+  const handleKeyPress = (event: React.KeyboardEvent<HTMLInputElement>) => {
+    if (event.key === 'Enter') {
+      event.preventDefault()
+      onSearch(searchQuery)
+    }
+  }
+
+  const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setSearchQuery(event.target.value)
+  }
+
+  useEffect(() => {
+    onSearch(searchQuery)
+  }, [searchQuery])
+
   return (
     <form className={styles.search}>
       <Input
         icon='search'
-        id={''}
+        id=''
         label='What do you dare to search?'
         placeholder='Nightmare quest'
         type='text'
+        value={searchQuery}
+        onChange={handleInputChange}
+        onKeyDown={handleKeyPress}
       />
     </form>
   )
