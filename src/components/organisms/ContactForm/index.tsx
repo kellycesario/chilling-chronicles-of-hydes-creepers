86371256a151396707de079dc30478d3ae330676
kellycesario <kellycesario@gmail.com>
2023-10-11 20:27:58 -0300

    feat: add formspree in contact form

--- src/components/organisms/ContactForm/index.tsx
+++ src/components/organisms/ContactForm/index.tsx
@@ -3,36 +3,52 @@
 import { Button } from '@/components/atoms/Button'
 import { Headings } from '@/components/atoms/Headings'
 import { Input } from '@/components/molecules/Input'
+import { Modal } from '@/components/molecules/Modal'
 import contact from '@/data/contact.json'
-import { useEffect } from 'react'
+import { useForm as useFormFormspree } from '@formspree/react'
+import { useEffect, useState } from 'react'
 import { useForm } from 'react-hook-form'
 import styles from './styles.module.scss'
 
 export const ContactForm = () => {
   const {
-    register,
-    handleSubmit,
-    formState: { errors },
+    handleSubmit: handleSubmitReactHookForm,
+    register: registerReactHookForm,
+    formState: { errors: errorsReactHookForm },
+    reset: resetReactHookForm,
   } = useForm()
 
-  const onSubmit = (data) => {
-    console.log('Form data submitted:', data)
-  }
+  const [state, handleSubmitFormspree] = useFormFormspree(
+    process.env.NEXT_PUBLIC_FORMSPREE_ID
+  )
+
+  const [isModalVisible, setModalVisible] = useState(false)
 
   useEffect(() => {
-    register('email', {
+    registerReactHookForm('email', {
       required: 'Email is required',
       pattern: {
         value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i,
         message: 'Invalid email address',
       },
     })
-  }, [register])
+  }, [registerReactHookForm])
+
+  const handleModalClose = () => {
+    setModalVisible(false)
+    resetReactHookForm()
+  }
 
   return (
     <section className={styles.contact}>
       <Headings align='center' children='Summon us' color='white' level='2' />
-      <form className={styles.contact__form} onSubmit={handleSubmit(onSubmit)}>
+      <form
+        className={styles.contact__form}
+        onSubmit={(event) => {
+          handleSubmitReactHookForm((data) => {})(event)
+          handleSubmitFormspree(event)
+        }}
+      >
         {contact.map((item, index) => (
           <div key={index} className={styles.inputContainer}>
             <Input
@@ -43,20 +59,24 @@ export const ContactForm = () => {
               placeholder={item.placeholder}
               type={item.type}
               error={
-                errors[item.id]?.message ||
-                (item.id === 'email' && errors['email']?.type === 'pattern'
+                errorsReactHookForm[item.id]?.message ||
+                (item.id === 'email' &&
+                errorsReactHookForm['email']?.type === 'pattern'
                   ? 'Invalid email address'
                   : null)
               }
               validation={item.validation}
               onChange={() => {}}
               onKeyDown={() => {}}
-              {...register(item.id, { required: item.validation?.required })}
+              {...registerReactHookForm(item.id, {
+                required: item.validation?.required,
+              })}
             />
           </div>
         ))}
         <Button type='submit' label='Submit' level='tertiary' />
       </form>
+      {state.succeeded && <Modal onClose={handleModalClose} />}
     </section>
   )
 }
