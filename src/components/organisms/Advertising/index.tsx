23f19147ec569babc350cd9de931e287cdd62053
kellycesario <kellycesario@gmail.com>
2023-10-19 17:02:03 -0300

    fix: adjustments in advertising component

--- src/components/organisms/Advertising/index.tsx
+++ src/components/organisms/Advertising/index.tsx
@@ -1,17 +1,21 @@
-import styles from './styles.module.scss'
+'use client'
+import styles from './styles.module.scss';
+import Script from 'next/script';
 
 export const Advertising = () => {
+  const scriptContent = `(adsbygoogle = window.adsbygoogle || []).push({});`;
+
   return (
     <section className={styles.advertising}>
       <ins
         className='adsbygoogle'
-        style={{ display: 'block' }}
+        style={{ display: 'block', minWidth: 300, minHeight: 250 }}
         data-ad-layout='in-article'
         data-ad-format='fluid'
         data-ad-client={`ca-pub-${process.env.NEXT_PUBLIC_GOOGLE_ADS_ID}`}
         data-ad-slot='6765860573'
       ></ins>
-      <script>(adsbygoogle = window.adsbygoogle || []).push({});</script>
+      <Script dangerouslySetInnerHTML={{ __html: scriptContent }} />
     </section>
-  )
-}
+  );
+};
