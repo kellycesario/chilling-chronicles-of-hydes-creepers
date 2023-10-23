babb90e4b07c7cd18e1073b73036214319196c3c
kellycesario <kellycesario@gmail.com>
2023-10-23 14:43:05 -0300

    fix: use google ads

--- src/utils/GoogleAds.tsx
+++ src/utils/GoogleAds.tsx
@@ -1,12 +1,11 @@
 'use client'
 
-import Script from 'next/script'
-
 export const GoogleAds = () => {
   return (
-    <Script
+    <script
       async
       src={`https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-${process.env.NEXT_PUBLIC_GOOGLE_ADS_ID}`}
-    />
+      crossOrigin='anonymous'
+    ></script>
   )
 }
