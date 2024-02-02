11993d1a4f64a5fd53cd581c0a36b7033a5c5d7e
kellycesario <kellycesario@gmail.com>
2024-02-02 18:34:48 -0300

    fix: update GoogleAnalyticsData.ts

--- src/utils/GoogleAnalyticsData.ts
+++ src/utils/GoogleAnalyticsData.ts
@@ -1,8 +1,13 @@
 const { BetaAnalyticsDataClient } = require('@google-analytics/data')
 
-const analyticsDataClient = new BetaAnalyticsDataClient()
-
 const propertyId = process.env.GA_PROPERTY_ID
+
+const analyticsDataClient = new BetaAnalyticsDataClient({
+  credentials: {
+    client_email: process.env.GA_CLIENT_EMAIL,
+    private_key: process.env.GA_PRIVATE_KEY?.replace(/\n/gm, '\n'),
+  },
+})
 
 async function runReport() {
   const [response] = await analyticsDataClient.runReport({
@@ -25,12 +30,9 @@ async function runReport() {
     ],
   })
 
-  console.log('Report result:')
   response.rows.forEach(
     (row: { dimensionValues: any[]; metricValues: any[] }) => {
       console.log(row.dimensionValues[0], row.metricValues[0])
     }
   )
 }
-
-runReport()
